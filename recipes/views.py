9aca44c2407a068201e2b6c9c70a910e7ac82976
Gerson Adriano Marques <gersonadrianomarques@gmail.com>
2022-05-24 17:10:19 -0300

    A27: inicio (removido contato e sobre)

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -7,11 +6,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Gerson Marques'
     })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE 1')
