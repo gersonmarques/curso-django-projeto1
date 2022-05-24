6662ca35a574fe31c4e3cf2541fd338f48b96a33
Gerson Adriano Marques <gersonadrianomarques@gmail.com>
2022-05-24 16:48:59 -0300

    A25: a função render e mais caminhos de templates

---
+++ recipes/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Gerson Marques'
+    })
+
+
+def contato(request):
+    return render(request, 'recipes/contato.html')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE 1')
